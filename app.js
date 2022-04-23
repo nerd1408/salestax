c30f0b016b46c95a881325561e09e9de8ecec916
maestrowscki10 <abdulhansen.ah@gmail.com>
2022-04-23 21:42:52 +0300

    deleted/clean code

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ function subTotal() {
     document.orderform.subtotal.value = productPrice.toFixed(2);
     return productPrice;
   }
-  //calculateTax() takes result of subTotal function but has to refer to the result to move forward as opposed to the previous function
-  //.toFixed() is the decimal points
+  /
   function calculateTax() {
-    //var subTotal = document.orderform.subtotal.value; OR for dryer code:
     var subtotal = subTotal();
     var stax = 0.03;
       tax = subtotal * stax;
     document.orderform.salestax.value = tax.toFixed(3);
     return tax;
   }
-  //takes the HTML output results from the previous two functions and adds them together. 
   function grandTotal() {
     var subtotal = subTotal();
     var tax = calculateTax();
